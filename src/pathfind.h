561e177a49033131e4b8245cb6cad335b2b38e29
CSjianbel <jiankarlo99@gmail.com>
2021-02-01 00:27:26 +0800

    Modified gitignore and pathfind.h : PATH & WALL values

--- src/pathfind.h
+++ src/pathfind.h
@@ -8,11 +8,10 @@
 #include <math.h>
 
 #define MAX_WIDTH 50
-#define WALL 219
-#define PATH 15
+#define WALL '#'
+#define PATH 'X'
 
 bool getDimension(char *path, int *height, int *width);
 void readBoard(char *path, int height, int width, Node *board[height][width], Node **start, Node **goal, bool pathing);
 void printBoard(int height, int width, Node *board[height][width]);
 bool findPath(int height, int width, Node *board[height][width], Node *start, Node *goal, char algorithm, bool pathing);
-
