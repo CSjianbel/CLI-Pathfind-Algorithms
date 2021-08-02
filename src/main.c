7b8674442ec778e0532b1706e5a28e35592d63c5
CSjianbel <jiankarlo99@gmail.com>
2021-08-02 21:39:32 +0800

    Added comments to main.c file to make the code easier to understand

--- src/main.c
+++ src/main.c
@@ -9,6 +9,11 @@ void err_args(char* extra_msg);
 
 int main(int argc, char **argv)
 {
+	/*
+	 * Default values:
+	 * 	pathing: true = diagonal 
+	 * 	algorithm: 'a' = Astar
+	 */
 	bool pathing = true;
 	char algorithm = 'a';
 	char* filepath = argv[1];
@@ -25,6 +30,7 @@ int main(int argc, char **argv)
 	}
 	fclose(test);
 
+	// Stores the dimensions of the given board
 	int width = 0, height = 0;
 
 	// Get the dimension of the given board
@@ -64,28 +70,36 @@ int main(int argc, char **argv)
  */
 void verifyArgs(int argc, char** argv, bool* pathing, char* algorithm, char** filepath)
 {
+	// Check if the user provided valid number of arguments
 	if (argc < 2 || argc > 4)
 	{
 		err_args("");
 		exit(1);
 	}
 	
+	// if the pathing, algorithm and board is specified
 	if (argc == 4)
 	{
+		// if the value for pathing is invalid
 		if (strcmp("-d", argv[1]) && strcmp("-a", argv[1]))
 		{	
 			err_args("");
 			exit(1);
 		}
 
+		// Set the value of pathing
+		// true : diagonal, false : across 
 		*pathing = !strcmp("-d", argv[1]) ? true : false;
 
+		// if the value for algorithm is invalid
 		if (strcmp(argv[2], "-a") && strcmp(argv[2], "-d") && strcmp(argv[2], "-b"))
 		{
 			err_args("");
 			exit(1);
 		}
 
+		// Set the value of algorithm
+		// a : A*, d : DFS. b : BFS
 		if (!strcmp(argv[2], "-a"))
 			*algorithm = 'a';
 		else if (!strcmp(argv[2], "-d"))
@@ -95,14 +109,18 @@ void verifyArgs(int argc, char** argv, bool* pathing, char* algorithm, char** fi
 
 		*filepath = argv[3];
 	} 
+	// if only the pathing and board is provided
 	else if (argc == 3)
 	{
+		// if the value for pathing is invalid
 		if (strcmp("-d", argv[1]) && strcmp("-a", argv[1]))
 		{	
 			err_args("");
 			exit(1);
 		}
 
+		// Set the value of pathing
+		// true : diagonal, false : across 
 		*pathing = !strcmp("-d", argv[1]) ? true : false;
 		*filepath = argv[2];
 	}
